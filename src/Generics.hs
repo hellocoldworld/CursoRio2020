493233e7d31e5f5d093bce0ac15c7d61825847e7
Nicolas Martinez <nicolas.martinez@poincenot.com>
2020-03-15 03:25:59 -0300

    implemented crush

--- src/Generics.hs
+++ src/Generics.hs
@@ -262,6 +262,25 @@ crush :: (Generic f, ..., Monoid m) => f m -> m
 
 -}
 
+class Crush Id where
+      crush :: (Monoid m) => f m -> m
+
+
+instance Crush Id where
+      crush (Id a) = a
+
+instance Crush (K b) where
+      crush (K b a) = mempty
+
+instance (Crush f, Crush g) => Crush (f :+: g) where
+      crush (Inl f) = crush f
+      crush (Inr g) = crush g
+
+instance (Crush f, Crush g) => Crush (f :*: g) where
+      crush (f :+: g) = (crush f) <> (crush g)
+
+gcrush :: (Generic f, Crush (Rep f), Monoid m) => f m -> m
+gcrush f = crush f
 --------------------------------------------------
 {- Tipos Recursivos -}
 
